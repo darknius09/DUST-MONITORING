6779c46b831978ccc6f9f77a77b77043eaf80413
darknight <darkniuslightness@gmail.com>
2025-02-16 06:56:18 +0700

    Update ENC28J60.ino

--- ENC28J60.ino
+++ ENC28J60.ino
@@ -1,67 +1,56 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <UIPEthernet.h>
-#include <SPI.h>
+#include <SoftwareSerial.h>
 #include <FS.h> // SPIFFS untuk penyimpanan data
 
 // Konfigurasi LCD I2C
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Konfigurasi Ethernet ENC28J60
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 100);
-EthernetServer server(80);
+// Konfigurasi CH9121 (Menggunakan SoftwareSerial)
+#define CH9121_TX D7
+#define CH9121_RX D6
+SoftwareSerial ethSerial(CH9121_RX, CH9121_TX); // RX, TX
 
 // Pin Konfigurasi
 #define DUST_SENSOR A0
-#define LED_SENSOR D0
 #define BUZZER D3
 #define RESET_BUTTON D4
 
 // Variabel Global
 float dustDensity = 0.0;
 bool alarmActive = false;
-unsigned long alarmResetTime = 0;
-const unsigned long resetDuration = 600000; // 10 menit dalam milidetik
-bool manualReset = false;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50;
+unsigned long alarmMuteStart = 0;
+bool alarmMuted = false;
 
 void setup() {
     Serial.begin(115200);
+    ethSerial.begin(19200); // Sesuaikan baud rate dengan CH9121
+
     Wire.begin();
     pinMode(DUST_SENSOR, INPUT);
-    pinMode(LED_SENSOR, OUTPUT);
     pinMode(BUZZER, OUTPUT);
     pinMode(RESET_BUTTON, INPUT_PULLUP);
-
+    
     lcd.init();
     lcd.backlight();
     lcd.setCursor(0, 0);
     lcd.print("Monitoring...");
 
-    Ethernet.begin(mac, ip);
-    server.begin();
-    Serial.println("Server Ethernet aktif");
-
     if (!SPIFFS.begin()) {
         Serial.println("Gagal mount SPIFFS");
     }
 
-    Serial.println("Ketik 'reset' untuk mematikan alarm sementara.");
+    Serial.println("Ketik 'reset' untuk mematikan alarm.");
 }
 
 void loop() {
-    // Aktifkan LED sensor sebelum membaca
-    digitalWrite(LED_SENSOR, HIGH);
-    delayMicroseconds(280); // Waktu sampling GP2Y1014AU
     int rawValue = analogRead(DUST_SENSOR);
-    delayMicroseconds(40);
-    digitalWrite(LED_SENSOR, LOW);
-
-    // Konversi ke ug/m3
     dustDensity = map(rawValue, 0, 1023, 0, 500);
 
     saveDataToSPIFFS(dustDensity);
-
+    
     lcd.setCursor(0, 1);
     lcd.print("Debu: ");
     lcd.print(dustDensity);
@@ -71,44 +60,33 @@ void loop() {
     Serial.print(dustDensity);
     Serial.println(" ug/m3");
 
-    // Cek kondisi alarm
-    if (dustDensity > 50 && !manualReset) {
+    if (dustDensity > 50 && !alarmMuted) {
+        digitalWrite(BUZZER, HIGH);
         alarmActive = true;
-        alarmResetTime = 0; // Reset timer
-    } 
-    else if (dustDensity < 50 && alarmResetTime == 0) {
+    } else if (dustDensity < 50 && alarmActive) {
+        digitalWrite(BUZZER, LOW);
         alarmActive = false;
     }
 
-    // Kontrol buzzer
-    if (alarmActive) {
-        digitalWrite(BUZZER, HIGH);
-    } else {
-        digitalWrite(BUZZER, LOW);
+    if (digitalRead(RESET_BUTTON) == LOW && (millis() - lastDebounceTime) > debounceDelay) {
+        lastDebounceTime = millis();
+        muteAlarm();
     }
 
-    // Cek tombol reset
-    if (digitalRead(RESET_BUTTON) == LOW) {
-        resetAlarm();
-    }
-
-    // Cek input dari Serial Monitor
     if (Serial.available() > 0) {
         String input = Serial.readStringUntil('\n');
         input.trim();
         if (input.equalsIgnoreCase("reset")) {
-            resetAlarm();
+            muteAlarm();
             while (Serial.available() > 0) Serial.read();
         }
     }
 
-    // Timer untuk reset manual 10 menit
-    if (manualReset && millis() - alarmResetTime >= resetDuration) {
-        manualReset = false;
-        Serial.println("Reset manual selesai, alarm aktif kembali.");
+    if (alarmMuted && millis() - alarmMuteStart >= 600000) {
+        alarmMuted = false;
+        Serial.println("Alarm kembali aktif.");
     }
 
-    handleClient();
     delay(2000);
 }
 
@@ -122,27 +100,10 @@ void saveDataToSPIFFS(float value) {
     file.close();
 }
 
-void handleClient() {
-    EthernetClient client = server.available();
-    if (client) {
-        client.println("HTTP/1.1 200 OK");
-        client.println("Content-Type: text/html");
-        client.println("Connection: close");
-        client.println();
-        client.println("<html><body>");
-        client.print("<h1>Monitoring Debu</h1>");
-        client.print("<p>Debu: ");
-        client.print(dustDensity);
-        client.println(" ug/m3</p>");
-        client.println("</body></html>");
-        client.stop();
-    }
-}
-
-void resetAlarm() {
-    alarmActive = false;
+void muteAlarm() {
     digitalWrite(BUZZER, LOW);
-    manualReset = true;
-    alarmResetTime = millis();
+    alarmActive = false;
+    alarmMuted = true;
+    alarmMuteStart = millis();
     Serial.println("Alarm dimatikan sementara selama 10 menit.");
 }
